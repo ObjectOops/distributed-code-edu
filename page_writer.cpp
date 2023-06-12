a5cd79e37b119f1163b45c60b5165847e6801940
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-12 01:02:26 +0000

    Add domain auto-detection.

--- page_writer.cpp
+++ page_writer.cpp
@@ -7,13 +7,24 @@
 
 using namespace std;
 
-const string DOMAIN {"example.com"}; // Enter domain here.
+string domain {"example.com"}; // Enter domain here. Empty for Codespace auto-detection.
 
 int main() {
+    if (domain.empty()) {
+        char *codespaceName        {getenv("CODESPACE_NAME")};
+        char *portForwardingDomain {getenv("GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN")};
+        if (codespaceName != nullptr && portForwardingDomain != nullptr) {
+            domain = string {codespaceName} + "-3000." + string {portForwardingDomain};
+        }
+    }
+    if (domain.empty()) {
+        cout << "No domain specified. Could not auto-detect.\n";
+        return 1;
+    }
     vector <string> directories {parseList("directories.txt")};
     string markdown {"# Please click on the link with your id.\n"};
     for (string &i : directories) {
-        markdown += "- [" + i + "](https://" + DOMAIN + "/?folder=/workspaces/distributed-code-edu/students/" + i + ")\n";
+        markdown += "- [" + i + "](https://" + domain + "/?folder=/workspaces/distributed-code-edu/students/" + i + ")\n";
     }
     ofstream fout {"index.md"};
     fout << markdown;
