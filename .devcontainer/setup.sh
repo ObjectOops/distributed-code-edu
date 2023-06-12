68bc1a65ae46f5309a2a9d21cd06b8e024bba2cd
ObjectOops <92550885+ObjectOops@users.noreply.github.com>
2023-06-12 00:31:30 +0000

    Apply editor layout modifications without using profile file.

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,6 +1,6 @@
 cd "/workspaces/$(ls /workspaces | head -n 1)/"
 sh install.sh
-cp simplify.code-profile /home/vscode/
+cp settings.json openvscode-server-v${OPENVSCODE_SERVER_VERSION}-linux-x64/server-data/data/Machine
 cd utilities
 g++ -w -O2 -std=c++17 extension_monitor.cpp -o extension_monitor.out
 g++ -w -O2 -std=c++17 create_directories.cpp -o create_directories.out
